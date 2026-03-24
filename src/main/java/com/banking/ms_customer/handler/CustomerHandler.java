e9c7b89ed2abe2f3e4183905faaef2d9fe1e013d
JoseVillacorta <villacortafur@gmail.com>
2026-03-24 02:21:27 -0500

    Update version routes and remove unused imports

--- src/main/java/com/banking/ms_customer/handler/CustomerHandler.java
+++ src/main/java/com/banking/ms_customer/handler/CustomerHandler.java
@@ -11,80 +11,86 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import javax.print.attribute.standard.Media;
 import java.net.URI;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class CustomerHandler {
 
-    private final CustomerService customerService;
+        private final CustomerService customerService;
 
-    // Obtener todos los clientes
-    public Mono<ServerResponse> findAll(ServerRequest request){
-        return customerService.findAll()
-                .collectList() //Agrupamos el flux en una lista para poder envolverla en ApiResponse
-                .flatMap(customers -> ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .bodyValue(ApiResponse.success(customers, "Ok", HttpStatus.OK.value()))
-                );
-    }
+        // Obtener todos los clientes
+        public Mono<ServerResponse> findAll(ServerRequest request) {
+                return customerService.findAll()
+                                .collectList() // Agrupamos el flux en una lista para poder envolverla en ApiResponse
+                                .flatMap(customers -> ServerResponse.ok()
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .bodyValue(ApiResponse.success(customers, "Ok",
+                                                                HttpStatus.OK.value())));
+        }
 
-    // Obtener un cliente por su ID
-    public Mono<ServerResponse> findById(ServerRequest request){
-        Long id = Long.valueOf(request.pathVariable("id"));
-        return customerService.findById(id)
-                .flatMap(c -> ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .bodyValue(ApiResponse.success(c, "Ok", HttpStatus.OK.value())))
-                .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND)
-                        .bodyValue(ApiResponse.error("Customer not found", HttpStatus.NOT_FOUND.value())));
-    }
+        // Obtener un cliente por su ID
+        public Mono<ServerResponse> findById(ServerRequest request) {
+                Long id = Long.valueOf(request.pathVariable("id"));
+                return customerService.findById(id)
+                                .flatMap(c -> ServerResponse.ok()
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .bodyValue(ApiResponse.success(c, "Ok", HttpStatus.OK.value())))
+                                .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND)
+                                                .bodyValue(ApiResponse.error("Customer not found",
+                                                                HttpStatus.NOT_FOUND.value())));
+        }
 
-    // Obtener un cliente por su dni
-    public Mono<ServerResponse> findByDocumentNumber(ServerRequest request){
-        String documentNumber = request.pathVariable("documentNumber");
-        return customerService.findByDocumentNumber(documentNumber)
-                .flatMap(c -> ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .bodyValue(ApiResponse.success(c, "OK", HttpStatus.OK.value())))
-                .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND)
-                        .bodyValue(ApiResponse.error("Customer not found", HttpStatus.NOT_FOUND.value())));
-    }
+        // Obtener un cliente por su dni
+        public Mono<ServerResponse> findByDocumentNumber(ServerRequest request) {
+                String documentNumber = request.pathVariable("documentNumber");
+                return customerService.findByDocumentNumber(documentNumber)
+                                .flatMap(c -> ServerResponse.ok()
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .bodyValue(ApiResponse.success(c, "OK", HttpStatus.OK.value())))
+                                .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND)
+                                                .bodyValue(ApiResponse.error("Customer not found",
+                                                                HttpStatus.NOT_FOUND.value())));
+        }
 
-    // Crear un nuevo cliente
-    public Mono<ServerResponse> create(ServerRequest request){
-        return request.bodyToMono(Customer.class)
-                .flatMap(customerService::create)
-                .flatMap(c -> ServerResponse
-                        .created(URI.create("/customers/" + c.getId()))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .bodyValue(ApiResponse.success(c, "Customer created successfully", HttpStatus.CREATED.value())));
-    }
+        // Crear un nuevo cliente
+        public Mono<ServerResponse> create(ServerRequest request) {
+                return request.bodyToMono(Customer.class)
+                                .flatMap(customerService::create)
+                                .flatMap(c -> ServerResponse
+                                                .created(URI.create("/customers/" + c.getId()))
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .bodyValue(ApiResponse.success(c, "Customer created successfully",
+                                                                HttpStatus.CREATED.value())));
+        }
 
-    // Actualizar un cliente existente
-    public Mono<ServerResponse> update(ServerRequest request){
-        Long id = Long.valueOf(request.pathVariable("id"));
+        // Actualizar un cliente existente
+        public Mono<ServerResponse> update(ServerRequest request) {
+                Long id = Long.valueOf(request.pathVariable("id"));
 
-        return request.bodyToMono(Customer.class)
-                .flatMap(c -> customerService.update(id, c))
-                .flatMap(c -> ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .bodyValue(ApiResponse.success(c, "Customer updated successfully", HttpStatus.OK.value())))
-                .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND)
-                        .bodyValue(ApiResponse.error("Customer not found", HttpStatus.NOT_FOUND.value())));
-    }
+                return request.bodyToMono(Customer.class)
+                                .flatMap(c -> customerService.update(id, c))
+                                .flatMap(c -> ServerResponse.ok()
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .bodyValue(ApiResponse.success(c, "Customer updated successfully",
+                                                                HttpStatus.OK.value())))
+                                .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND)
+                                                .bodyValue(ApiResponse.error("Customer not found",
+                                                                HttpStatus.NOT_FOUND.value())));
+        }
 
-    // Eliminar un cliente
-    public Mono<ServerResponse> delete(ServerRequest request){
-        Long id = Long.valueOf(request.pathVariable("id"));
-        return customerService.findById(id)
-                .flatMap(c -> customerService.delete(id)
-                    .then(ServerResponse.ok()
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .bodyValue(ApiResponse.success(null, "Customer deleted successfully", HttpStatus.OK.value()))))
-                .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND)
-                        .bodyValue(ApiResponse.error("Customer not found", HttpStatus.NOT_FOUND.value())));
-    }
+        // Eliminar un cliente
+        public Mono<ServerResponse> delete(ServerRequest request) {
+                Long id = Long.valueOf(request.pathVariable("id"));
+                return customerService.findById(id)
+                                .flatMap(c -> customerService.delete(id)
+                                                .then(ServerResponse.ok()
+                                                                .contentType(MediaType.APPLICATION_JSON)
+                                                                .bodyValue(ApiResponse.success(null,
+                                                                                "Customer deleted successfully",
+                                                                                HttpStatus.OK.value()))))
+                                .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND)
+                                                .bodyValue(ApiResponse.error("Customer not found",
+                                                                HttpStatus.NOT_FOUND.value())));
+        }
 }
